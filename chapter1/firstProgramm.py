84e66eaa68e54251d075e7c7fd0c60c0e7a84d5d
EvgeniiPliusnin <ea.plyusnin@gmail.com>
2022-06-23 20:45:50 +0400

    added folder test and chapter1

---
+++ chapter1/firstProgramm.py
@@ -0,0 +1,8 @@
+#this programm prints hello and asks user's name
+
+print('hello, world')
+print('what is your name ?')    # request for take name
+myName = input()E
+print('Nice to meet you, ' + myName)
+print('length of your name is: ')
+print(len(myName), 'letters')
